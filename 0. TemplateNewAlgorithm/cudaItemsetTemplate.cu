db72524e79daadc07086c6705d3894982e0df413
David <ferrufinoda2@vcu.edu>
2025-03-11 14:44:48 -0400

    Changed template file to be more consistent

--- 0. TemplateNewAlgorithm/cudaItemsetTemplate.cu
+++ 0. TemplateNewAlgorithm/cudaItemsetTemplate.cu
@@ -21,18 +21,10 @@ typedef struct {
     int firstChild; 
 } Node; 
 
-// Calculates the distance between two instances
-__device__ float generateItemSet(float* instance_A, float* instance_B, int num_attributes) {
-    float sum = 0;
-    
-    for (int i = 0; i < num_attributes-1; i++) {
-        float diff = instance_A[i] - instance_B[i];
-        //printf("instance a and b were %.3f %.3f\n", instance_A[i] ,instance_B[i]);
-        sum += diff*diff;
-    }
-    //printf("sum was %.3f\n,", sum);
-    return sqrt(sum);
-}
+typedef struct {
+    int id;
+    int* bitmap;
+} ItemBitmap;
 
 __global__ void processItemSets(char *inData, int minimumSetNum, int *d_Offsets, int totalRecords, int blocksPerGrid) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
@@ -45,9 +37,11 @@ __global__ void processItemSets(char *inData, int minimumSetNum, int *d_Offsets,
     int itemCount = 0;
     int number = 0;
     int items[32];
+
+
     // Initialize the shared memory (done by thread 0 in each block)
-    if (tid <= 9) {
-        printf("are are in tid %d\n", tid);
+    if (tid <= 10000) {
+        printf("we are in tid %d\n", tid);
         //Extract items from the input line
         for (char* current = line; *current != '\n' && *current != '\0'; current++) {
             if (*current >= '0' && *current <= '9') {
@@ -69,7 +63,7 @@ __global__ void processItemSets(char *inData, int minimumSetNum, int *d_Offsets,
         }
         for(int i = 0; i < itemCount; i++){
             printf("%d", items[i]);
-            
+
         }
         
         
@@ -85,14 +79,132 @@ __global__ void processItemSets(char *inData, int minimumSetNum, int *d_Offsets,
     
 }
 
+/* This is a helper function if you come across a study that uses a bitmap
+This function generates the bitmap (But only on the CPU)*/
+int* generateBitmapCPU(char* h_buffer, int* h_offsets){
+
+    /* So we are reading in every line
+    We must find a way to generate the vertical database
+    It is known the total number of possible items is 181682 (i counted this)
+    */
+
+    /* for the t100 database let's do that calculation
+    100,000 transactions / 32 bits and we get 3125 numbers requiredto store that 
+
+    we want to keep our arrays 1-D (best memory eff) so let's say 
+
+    1000 items (known) * 3125 = 325000 total required array length
+    
+    */
+
+    //keeping track of our location of where we are in the bitmap
+    //we know we need 1718 ints because we have 55012 transactions, so we do 55012 / 32 bits and get 1717.12 but we need 1718 to cover the remainder
+    int* itemsBitmap = (int*)calloc(3125000, sizeof(int));
+    int rowSize = 3125; //already known 
+    int countInBitmap = 0;
+    //ItemBitmap firstBitmap[181682]; 
+    bool inNumber = false;
+    int itemCount = 0;
+
+    
+    int number = 0;
+   
+    int countOfItems = 0;
+ 
+    //going through the database line by line
+    for(int i = 0; i < 100000; i++){
+        //printf("line %d\n", i);
+        int locationOfTransaction = (rowSize) - (i/32);
+        //code for where to flip the bit 
+        //  = (i / 32) + ( i % 32)  
+        number = 0;
+        for (char* current = h_buffer + h_offsets[i]; *current != '\n' && *current != '\0'; current++) {
+            //code for the bit flip
+            // |= (1 << (i - 1))
+            
+            if (*current >= '0' && *current <= '9') {
+                //printf("test");
+                number = number * 10 + (*current - '0');
+                inNumber = true;
+            } else if (inNumber) {
+                int locationOfInsertion = locationOfTransaction + (number * rowSize); 
+                if(number == 999){
+                    //printf("We found item 999 at %d\n", i);
+                    //printf("the location of insertion will be %d\b\n", locationOfInsertion);
+                }
+                
+                
+                //printf("Are we gonna segfault? + locaiton of insertion %d and number is %d\n", locationOfInsertion, number);
+                itemsBitmap[locationOfInsertion] |= (1 << (i % 32));
+                // firstBitmap[countInBitmap].id =  number;
+                // firstBitmap[countInBitmap].bitmap[location] |= (1 << (i % 32));
+                countInBitmap++;
+                //printf("%d\n",number);
+                countOfItems++;
+                //items[itemCount] = number;
+                itemCount++;
+                number = 0; 
+                inNumber = false;
+                
+            }
+
+
+            
+            //printf("\n");
+        }
+       
+        //printf("not segfaulted\n");
+        if(number == 999){
+            printf("We found item 999 at %d\n", i);
+        }
+        // firstBitmap[countInBitmap].id =  number;
+        // firstBitmap[countInBitmap].bitmap[location] |= (1 << (i % 32));
+        int locationOfInsertion = locationOfTransaction + (number * rowSize);   
+        //printf("Are we gonna segfault OUT OF INNER? + locaiton of insertion %d\n", locationOfInsertion);
+        itemsBitmap[locationOfInsertion]  |= (1 << (i % 32)); 
+        countInBitmap++;
+        //printf("%d\n", number);
+        countOfItems++;
+
+       
+    }
+    return itemsBitmap;
+}
+
+/* This is a helper function if you come across a study that uses a bitmap
+This function prints a bitmap*/
+void printOutBitmap(int* itemsBitmap){
+    for(int i = 0; i < 1000; i++){
+            if( i == 999){
+                printf("\n Item %d: ", i);
+                for(int j = 0; j < 3125; j++){
+                        
+                    int temp = itemsBitmap[i * 3125 + j];
+
+
+                        int position = 100000 - (j*32);
+                        int locationtracker = i * 3125 + j;
+                        if(itemsBitmap[i * 3125 + j] != 0){
+                            int temp = itemsBitmap[i * 3125 + j] & -itemsBitmap[i * 3125 + j];
+                            int index = __builtin_ctz(temp); // Get index of LSB (0-based)
+                            printf("Bit at index: %d\n", index);
+                            printf("%d, tid %d , the location (which should match insertion where this is should be %d |||| " , itemsBitmap[locationtracker], position + index + 1, locationtracker);
+                        }
+                }
+            }
+        }
+}
+
 // Implements a threaded kNN where for each candidate query an in-place priority queue is maintained to identify the nearest neighbors
 int KNN() {   
+    printf("we started\n");
     clock_t cpu_start_withSetup = clock();
     
     clock_t setupTimeStart = clock();
-    //int lineCountInDataset = 1692081;
-    int lineCountInDataset = 55012;
-    const char* inDataFilePath = "../sortedDataBase.txt";
+    
+
+    int lineCountInDataset = 100000;
+    const char* inDataFilePath = "../T10I4D100K.txt";
 
     FILE* file = fopen(inDataFilePath, "r");
 
@@ -119,13 +231,30 @@ int KNN() {
         }
     }
     
+    int* itemBitmap = generateBitmapCPU(h_buffer, h_offsets);
+    printOutBitmap(itemBitmap);
+
     // Allocate memory to hold the file contents
+    bool inNumber = false;
+
     char* h_text = (char*)malloc(file_size);
+    int itemCount = 0;
+    //int number = 0;
+    
 
     // Read the file into the host buffer
     fread(h_text, 1, file_size, file);
+
+
     //fclose(file);
     //size_t sharedMemSize = (6 * MAX_NODES) * sizeof(int) +  1 * sizeof(int) ;  // 5 arrays + nodeCounter
+
+    
+    
+
+    printf("we escaped the for loop");
+    
+   
     
     // Allocate memory on the GPU
     char* d_text;
@@ -157,7 +286,7 @@ int KNN() {
 
     
     cudaEventRecord(startEvent);
-    processItemSets<<<blocksPerGrid, threadsPerBlock>>>(d_text, minItemCount, d_offsets, lineCountInDataset, blocksPerGrid);
+    //processItemSets<<<blocksPerGrid, threadsPerBlock>>>(d_text, minItemCount, d_offsets, lineCountInDataset, blocksPerGrid);
     cudaDeviceSynchronize();
     cudaEventRecord(stopEvent);
     cudaEventSynchronize(stopEvent);
@@ -209,7 +338,7 @@ int KNN() {
 int main(int argc, char *argv[])
 {
     
-
+    printf("test\n");
     int x = KNN();
     return -1;  
 }
